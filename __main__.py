b15fb1a8863f61bef80950dcf2e1b9ac173c088c
sijianneu <wang.sijian1@northeastern.edu>
2023-10-24 22:52:17 -0400

    assignment6

--- __main__.py
+++ __main__.py
@@ -20,6 +20,10 @@ amiId = config.require("amiId")
 sshkeyName = config.require("sshkeyName")
 instanceName = config.require("instanceName")
 appSecurityGroup = config.require("appSecurityGroup")
+rdsPassword = config.require("rdsPassword")
+rdsUsername = config.require("rdsUsername")
+rsdIdentifier = config.require("rsdIdentifier")
+databaseName = config.require("databaseName")
 
 # get AZ
 available_az = aws.get_availability_zones(state="available").names
@@ -40,6 +44,8 @@ subnets_list = list(ip_network(vpcCidrBlock).subnets(new_prefix=24))
 vpc = aws.ec2.Vpc(vpcName,
     cidr_block = vpcCidrBlock,
     instance_tenancy = "default",
+    enable_dns_hostnames=True,
+    
     tags = {
         "Name": vpcName,
     })
@@ -60,6 +66,8 @@ private_route_table = aws.ec2.RouteTable(privateRouteTableName, vpc_id=vpc.id,
 
 #save subnets created in list
 created_publicsubnets =[]
+created_privatesubnets =[]
+created_privatesubnetsIds= []
 #create public subnets
 for az_index in range(ind_range):
     public_subnet = aws.ec2.Subnet(publicSubnetsName + str(az_index),
@@ -88,8 +96,15 @@ for az_index in range(ind_range):
                                                        route_table_id=private_route_table.id,
                                                        subnet_id=private_subnet.id)
     
+    created_privatesubnets.append(private_subnet)
+    created_privatesubnetsIds.append(private_subnet.id)
     ind_cidr += 1
-    
+
+# Create a DB Subnet Group
+db_subnet_group = aws.rds.SubnetGroup("my-db-subnet-group",
+    subnet_ids=created_privatesubnetsIds,
+    description="private DB subnet group for rds"
+)
     
 #add public cidr destination and gateway
 public_route = aws.ec2.Route(
@@ -99,6 +114,7 @@ public_route = aws.ec2.Route(
     gateway_id=internet_gateway.id,
 )
 
+#security group for instance
 app_security_group = aws.ec2.SecurityGroup(appSecurityGroup,
     description='EC2 security group for web applications',
     vpc_id=vpc.id,
@@ -130,24 +146,101 @@ app_security_group = aws.ec2.SecurityGroup(appSecurityGroup,
             from_port=8080,
             to_port=8080,
             cidr_blocks=['0.0.0.0/0'],
-        ),
-    ]
+        )],
+    egress=[aws.ec2.SecurityGroupEgressArgs(
+        from_port=0,
+        to_port=0,
+        protocol="-1",
+        cidr_blocks=["0.0.0.0/0"],
+        ipv6_cidr_blocks=["::/0"],
+    )],
+     tags={
+        "Name": "Security group for ec2",
+    }
 )
 
+#security group for rds
+database_security_group = aws.ec2.SecurityGroup("databaseSecurityGroup",
+    description="My RDS security group",
+    vpc_id=vpc.id,
+    egress=[
+        # Allow all outbound traffic.
+        {
+            "protocol": "-1",
+            "from_port": 0,
+            "to_port": 0,
+            "cidr_blocks": ["0.0.0.0/0"],
+        }
+    ],
+    tags={"Name": "DatabaseSecurityGroup"})
+
+# Add an ingress rule for MariaDB
+mysql_ingress = aws.ec2.SecurityGroupRule("mysqlIngressRule",
+    type="ingress",
+    from_port=3306,
+    to_port=3306,
+    protocol="tcp",
+    security_group_id=database_security_group.id,
+    source_security_group_id=app_security_group.id) 
+
+db_parameter_group = aws.rds.ParameterGroup('csye6225-db-param-group',
+                                            family='mariadb10.6',
+                                            description='Custom Parameter Group for CSYE6225',)
+
+
+my_rds = aws.rds.Instance("SQLInstance",
+        engine="MariaDB",
+        instance_class="db.t3.micro",
+        allocated_storage=20,
+        db_name=databaseName,
+        multi_az=False,
+        identifier=rsdIdentifier,
+        username=rdsUsername,
+        password=rdsPassword,
+        # publicly_accessible=True,
+        db_subnet_group_name=db_subnet_group,
+        parameter_group_name=db_parameter_group.name,
+        skip_final_snapshot=True,
+        vpc_security_group_ids=[database_security_group.id])
+
+user_data_content = my_rds.endpoint.apply(lambda endpoint:f"""#!/bin/bash
+ENV_FILE="/etc/systemd/system/service.env"
+sudo sh -c "echo 'DATABASE_URL=jdbc:mysql://{endpoint}/{databaseName}?createDatabaseIfNotExist=true' >> ${{ENV_FILE}}"
+sudo sh -c "echo 'DATABASE_USER={rdsUsername}' >> ${{ENV_FILE}}"
+sudo sh -c "echo 'DATABASE_PASSWORD={rdsPassword}' >> ${{ENV_FILE}}"
+
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo chown csye6225:csye6225 /opt/artifact/cloudwebapp-0.0.1-SNAPSHOT.jar
+sudo chown csye6225:csye6225 /etc/systemd/system/service.env
+sudo chown csye6225:csye6225 /opt/users.csv
+
+sudo systemctl daemon-reload
+sudo systemctl enable app.service
+sudo systemctl stop cloud-final.service
+sudo systemctl start cloud-final.service
+sudo systemctl start cloud-init.target
+sudo systemctl start app.service
+""")
+
+#create app instance
 app_instance = aws.ec2.Instance(instanceName,
+    opts=pulumi.ResourceOptions(depends_on=[my_rds]),
     ami=amiId,  # AMI ID created by workflow
     instance_type='t2.micro', 
     # security_groups=[app_security_group.name], 
     vpc_security_group_ids = [app_security_group.id],
     subnet_id=created_publicsubnets[0].id,
-    disable_api_termination=False,  # No protection against accidental termination
+    disable_api_termination=False,# No protection against accidental termination
     root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
         volume_size=25,
         volume_type='gp2',
         delete_on_termination=True  # ensure EBS volume is terminated with the instance
     ),
     key_name=sshkeyName,
+    user_data= user_data_content,
     tags={
         'Name': instanceName,
     }
 )
+
