9444cfe4215300b054a742cac1fa83aaa83a5cd3
sijianneu <114265018+sijianneu@users.noreply.github.com>
2023-10-25 19:06:16 -0400

    submit2 (#5)
    * modify
    
    * rewrite by py
    
    * Update .gitignore
    
    * rewrite by py
    
    * Update .gitignore
    
    * rewrite by py
    
    * Update README.md
    
    * instance
    
    * assignment6
    
    * submit2
    
    ---------
    
    Co-authored-by: Sijian Wang <sijian250@gmail.com>

--- __main__.py
+++ __main__.py
@@ -116,7 +116,6 @@ public_route = aws.ec2.Route(
     gateway_id=internet_gateway.id,
 )
 
-
 #security group for instance
 app_security_group = aws.ec2.SecurityGroup(appSecurityGroup,
     description='EC2 security group for web applications',
@@ -214,15 +213,19 @@ sudo sh -c "echo 'DATABASE_PASSWORD={rdsPassword}' >> ${{ENV_FILE}}"
 
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo chown csye6225:csye6225 /opt/artifact
 sudo chown csye6225:csye6225 /opt/artifact/cloudwebapp-0.0.1-SNAPSHOT.jar
 sudo chown csye6225:csye6225 /etc/systemd/system/service.env
+sudo chown csye6225:csye6225 /etc/systemd/system/app.service
 sudo chown csye6225:csye6225 /opt/users.csv
+sudo chmod 744 /opt/users.csv
+sudo chmod 744 /etc/systemd/system/app.service
+sudo chmod 744 /etc/systemd/system/service.env
+sudo chmod 744 /opt/artifact/cloudwebapp-0.0.1-SNAPSHOT.jar
 
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 sudo systemctl stop cloud-final.service
-sudo systemctl start cloud-final.service
-sudo systemctl start cloud-init.target
 sudo systemctl start app.service
 """)
 
